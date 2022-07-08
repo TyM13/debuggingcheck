8bd779aa03bc7e1856cef420b983859c64b82404
Your Name <you@example.com>
2022-07-07 20:43:34 -0600

    jsarrays knowledge check

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
-let username = "tyler";
-let user_age = 28;
-let is_member = "false";
+// let username = "tyler";
+// let user_age = 28;
+// let is_member = "false";
 
 
-if (username = "admin") {
-    console.log("special greeting");
-}else if  (user_age >= 30 && is_member === "true") {
-    console.log("special greeting!");
-} else if(user_age < 30 && is_member === "false") {
-    console.log("greeting");
-}else{
-    console.log("not met");
-}
+// if (username = "admin") {
+//     console.log("special greeting");
+// }else if  (user_age >= 30 && is_member === "true") {
+//     console.log("special greeting!");
+// } else if(user_age < 30 && is_member === "false") {
+//     console.log("greeting");
+// }else{
+//     console.log("not met");
+// }
+
+
+
+// let username = `tyler`;
+// let user_age = `twenty`;
+
+// if (username = `tyler`) {
+//     console.log(`hello, tyler you are twenty`)
+// }
+
+// let max_volume = 30;
+// let current_volume =15;
+
+// console.log(current_volume / max_volume);
+// console.log(current_volume / max_volume *100)
+
+
+
+let usernames = [`ty`, `tyler`, `mersereau`, `tym`, `tylermersereau`];
+let user_ages = [10, 20, 30, 40, 50]
