a3d9609ceb99a9e7bd21603add59430a356b1d9f
Your Name <you@example.com>
2022-07-07 19:45:15 -0600

    probelms with debug knowledgecheck

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let username = "tyler";
+let user_age = 28;
+let is_member = "false";
+
+
+if (username = "admin") {
+    console.log("special greeting");
+}else if  (user_age >= 30 && is_member === "true") {
+    console.log("special greeting!");
+} else if(user_age < 30 && is_member === "false") {
+    console.log("greeting");
+}else{
+    console.log("not met");
+}
